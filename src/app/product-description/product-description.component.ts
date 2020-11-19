546c849d16ce29502b5f0d8bba79375cdde8de6f
Zia Malik <ziamalik@gmail.com>
2020-11-19 04:39:03 -0500

    Added Album interface

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Album } from '../album';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
+
 
 @Injectable()
 export class ProductService {
@@ -10,8 +14,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response : Response) => response.json());
+  getAlbum(id: number) : Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 
 }
